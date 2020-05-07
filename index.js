095ce8547e29b468114e00cdef689c0203bb03b0
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 21:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function logShout(string) {console.log(string.toUpperCase())
 function logWhisper(string) {console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {if (string = string.toLowerCase()) {return "I can't hear you!"}
-else if (string = string.toUpperCase())
- {return "YES INDEED!"}
+function sayHiToGrandma(string) {if (string === string.toLowerCase()) {return "I can't hear you!"}
+else if (string === string.toUpperCase()) {return "YES INDEED!"}
 }
